4ae4aa4040ff721b8ce3ecf3f20c9a84512deb67
mjilani98 <mjilani@emich.edu>
2025-03-07 12:09:12 -0500

    modification

--- app/src/main/java/com/example/hw3q1/MainActivity.java
+++ app/src/main/java/com/example/hw3q1/MainActivity.java
@@ -25,29 +25,68 @@ public class MainActivity extends AppCompatActivity {
         //get the current principal
         EditText edtTxtCurrentPrincipal = findViewById(R.id.InputPrincipal);
         String strCurrentPrincipal = edtTxtCurrentPrincipal.getText().toString();
-        int currentPrincipal = Integer.parseInt(strCurrentPrincipal);
+        int currentPrincipal;
+        try
+        {
+             currentPrincipal = Integer.parseInt(strCurrentPrincipal);
+        }
+        catch (NumberFormatException e)
+        {
+            currentPrincipal = 0;
+        }
 
         //get the annual addition
         EditText edtTxtAnnualAddition = findViewById(R.id.InputAnnualAddition);
         String strAnnualAddition = edtTxtAnnualAddition.getText().toString();
-        int annualAddition = Integer.parseInt(strAnnualAddition);
+        int annualAddition ;
+        try
+        {
+            annualAddition = Integer.parseInt(strAnnualAddition);
+        }
+        catch (NumberFormatException e)
+        {
+            annualAddition =0;
+        }
 
         //get number of years
         EditText edtTxtNumberOfYears = findViewById(R.id.inputNumberYears);
         String strNumberYears = edtTxtNumberOfYears.getText().toString();
-        int numberOfYears = Integer.parseInt(strNumberYears);
+        int numberOfYears ;
+        try
+        {
+             numberOfYears = Integer.parseInt(strNumberYears);
+        }
+        catch (NumberFormatException e)
+        {
+            numberOfYears= 0;
+        }
+
+
+        //get the investment rate
+        EditText edtTxtInvestmentRate = findViewById(R.id.InputInvestmentRate);
+        String strInvestmentRate = edtTxtInvestmentRate.getText().toString();
+        int investmentRate ;
+        try
+        {
+            investmentRate = Integer.parseInt(strInvestmentRate);
+        }
+        catch (NumberFormatException e)
+        {
+            investmentRate= 0;
+        }
+
 
         //create a second activity
-        Intent secondActivity = new Intent(this,com.example.hw3q1.secondActivity.class);
+        Intent secondActivity = new Intent(this,secondActivity.class);
 
-        //put current principal , annual addition, and number of years as extras
+        //put current principal , annual addition, number of years, and investment rate as extras
         //to the second activity
         secondActivity.putExtra("currentPrincipal",currentPrincipal);
         secondActivity.putExtra("annualAddition",annualAddition);
         secondActivity.putExtra("numberOfYears",numberOfYears);
+        secondActivity.putExtra("investmentRate",investmentRate);
 
         //call the second activity
-        startActivity(secondActivity);
-        
+//        startActivity(secondActivity);
     }
 }
--- app/src/main/java/com/example/hw3q1/secondActivity.java
+++ app/src/main/java/com/example/hw3q1/secondActivity.java
@@ -1,4 +1,56 @@
 package com.example.hw3q1;
 
-public class secondActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+public class secondActivity extends AppCompatActivity
+{
+
+    //set second screen
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //display the second screen
+        setContentView(R.layout.activity_second);
+
+        //update the screen
+//        updateView();
+    }
+
+    public void updateView()
+    {
+        //get the inputs from extras
+        Intent intent = getIntent();
+        int currentPrincipal = intent.getIntExtra("currentPrincipal",0);
+        int annualAddition = intent.getIntExtra("annualAddition",0);
+        int numberOfYears = intent.getIntExtra("numberOfYears",0);
+        int investmentRate = intent.getIntExtra("investmentRate",0);
+
+        String line="here";
+        int total = currentPrincipal;
+        //do calculation
+        for(int x = 0 ; x < numberOfYears ; x++ )
+        {
+            line += (x+1)+"     " + total+"\n";
+            total+= annualAddition;
+            int investRetur = total*(investmentRate/100);
+            total +=investRetur;
+        }
+
+        //display the result of calculation
+        TextView output = findViewById(R.id.outputResults);
+        output.setText(line);
+    }
+
+    //even handler to destroy the current screen
+    public void back(View v)
+    {
+        //destroy the screen
+        finish();
+    }
 }
