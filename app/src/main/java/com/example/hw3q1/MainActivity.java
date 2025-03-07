15a140e239da6813154451445786cbe455f87f8e
mjilani98 <mjilani@emich.edu>
2025-03-07 11:10:05 -0500

    Create a secondActiviy.java class Modified a MainActivity.java

--- app/src/main/java/com/example/hw3q1/MainActivity.java
+++ app/src/main/java/com/example/hw3q1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.hw3q1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void calculate(View view)
+    {
+        //get the current principal
+        EditText edtTxtCurrentPrincipal = findViewById(R.id.InputPrincipal);
+        String strCurrentPrincipal = edtTxtCurrentPrincipal.getText().toString();
+        int currentPrincipal = Integer.parseInt(strCurrentPrincipal);
+
+        //get the annual addition
+        EditText edtTxtAnnualAddition = findViewById(R.id.InputAnnualAddition);
+        String strAnnualAddition = edtTxtAnnualAddition.getText().toString();
+        int annualAddition = Integer.parseInt(strAnnualAddition);
+
+        //get number of years
+        EditText edtTxtNumberOfYears = findViewById(R.id.inputNumberYears);
+        String strNumberYears = edtTxtNumberOfYears.getText().toString();
+        int numberOfYears = Integer.parseInt(strNumberYears);
+
+        //create a second activity
+        Intent secondActivity = new Intent(this,com.example.hw3q1.secondActivity.class);
+
+        //put current principal , annual addition, and number of years as extras
+        //to the second activity
+        secondActivity.putExtra("currentPrincipal",currentPrincipal);
+        secondActivity.putExtra("annualAddition",annualAddition);
+        secondActivity.putExtra("numberOfYears",numberOfYears);
+
+        //call the second activity
+        startActivity(secondActivity);
+        
     }
 }
---
+++ app/src/main/java/com/example/hw3q1/secondActivity.java
@@ -0,0 +1,4 @@
+package com.example.hw3q1;
+
+public class secondActivity {
+}
